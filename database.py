07039e8580170103a11d10c79ebd0084aabcc644
charlottedavidson524 <charlottedavidson256@gmail.com>
2025-11-28 12:52:24 +0000

    Database updated and models commit.

--- database.py
+++ database.py
@@ -4,27 +4,54 @@ class Database:
     """
     Create database layer.
     """
-    def __init__(self, db_name = "library.db"):
+    def __init__(self, db_name: str = "library.db"):
+        """
+        Initialise a database.
+
+        Args:
+            db_name (str): Name of database.
+        """
         self.conn = sqlite3.connect(db_name)
         self.create_table()
 
     def create_table(self):
         """
+        Create a SQL table.
         """
-        pass
+        query = """
+        CREATE TABLE IF NOT EXISTS library (
+            id INTEGER PRIMARY KEY AUTOIMPLEMENT
+            author TEXT NOT NULL
+            title TEXT NOT NULL
+        )
+        """
+        self.conn.execute(query)
+        self.conn.commit()
 
-    def insert_book(self):
+    def insert_book(self, author: str, title: str):
         """
+        Create book data.
+
+        Args:
+            author (str): Author's name.
+            title (str): Book title.
         """
-        pass
+        query = "INSERT INTO library (author, title) VALUES (?, ?)"
+        self.conn.execute(query, (author, title))
+        self.conn.commit()
 
     def fetch_book(self):
         """
+        Pull back all library records/list of books.
         """
-        pass
+        query = "SELECT * FROM library"
+        cursor = self.conn.execute(query)
+        return cursor.fetchall()
 
-    def delete_book(self):
+    def delete_book(self, book_id):
         """
+        Delete record of a book.
         """
-        pass
+        self.conn.execute("DELETE FROM contacts WHERE id= ?", (book_id,))
+        self.conn.commit()
 
