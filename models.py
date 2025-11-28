ae7bca442cc73eff0deb3293b1f741332441d26b
charlottedavidson524 <charlottedavidson256@gmail.com>
2025-11-28 12:56:13 +0000

    Model code.

--- models.py
+++ models.py
@@ -1,1 +1,7 @@
+class Book:
+    def __init__(self, author, title):
+        self.author = author
+        self.title = title
 
+    def __str__(self):
+        return f"{self.author} - {self.title}"
