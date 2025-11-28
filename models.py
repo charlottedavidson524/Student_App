dfe0d5896a86176776019182de502ef15ef4a19d
charlottedavidson524 <charlottedavidson256@gmail.com>
2025-11-28 12:59:25 +0000

    Commented code.

--- models.py
+++ models.py
@@ -1,7 +1,20 @@
 class Book:
-    def __init__(self, author, title):
+    def __init__(self, author: str, title: str):
+        """
+        Initialise a new book instance.
+
+        Args:
+            author (str): The name of the book's author.
+            title (str): The title of the book.
+        """
         self.author = author
         self.title = title
 
     def __str__(self):
+        """
+        Return a human readable string representation of the book.
+
+        Returns:
+            str: A string in the format "author - title".
+        """
         return f"{self.author} - {self.title}"
